0364fa99a4ac2d48f5e336c165222a563020d73f
Björn Strömberg <bjeeeern@gmail.com>
2024-03-01 21:01:50 +0100

    refactor testsuite

--- Test/Program.cs
+++ Test/Program.cs
@@ -3,12 +3,29 @@ using Product;
 
 Console.OutputEncoding = Encoding.UTF8;
 
-var userRegister = new UserRegister();
-
-if (userRegister.Users.Count != 0) throw new Exception();
-
-userRegister.RegisterUser("test.testsson", "qwerty123456");
-
-if (userRegister.Users.Count != 1) throw new Exception();
+CanRegisterUser();
 
 Console.WriteLine("✅ All tests OK ✅");
+
+static void CanRegisterUser()
+{
+    var userRegister = new UserRegister();
+
+    AssertEmpty(userRegister.Users);
+
+    userRegister.RegisterUser("test.testsson", "qwerty123456");
+
+    AssertSingle(userRegister.Users);
+}
+
+static void AssertEmpty<T>(IEnumerable<T>? enumeration)
+{
+    if (enumeration?.Any() ?? true)
+        throw new Exception($"Expected to be empty but found {enumeration?.Count()} elements.");
+}
+
+static void AssertSingle<T>(IEnumerable<T>? enumeration)
+{
+    if (enumeration?.Count() != 1)
+        throw new Exception($"Expected single element but found {enumeration?.Count()} elements.");
+}
